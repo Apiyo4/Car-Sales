865da4e7b6a3d5d670d258f52f2180cbf89cc884
Annette Odingo <odingoannette@gmail.com>
2019-12-12 05:00:10 +0000

    Refactor code to have one slice

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,26 +1,51 @@
 import * as types from './actionTypes';
+import { type } from 'os';
 
-const initialAdditionalPrice = 0;
-export function additionalPriceReducer(additionalPrice=initialAdditionalPrice, action){
-    return additionalPrice;
-}
-
-const initialCar =  {
+const initialState = {
+  additionalPrice: 0,
+  car: {
     price: 26395,
     name: '2019 Ford Mustang',
     image:
       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
     features: []
-  }
-  export function carReducer(car=initialCar, action){
-      return car;
-  }
-  const initialAdditionalFeatures = [
+  },
+  additionalFeatures: [
     { id: 1, name: 'V-6 engine', price: 1500 },
     { id: 2, name: 'Racing detail package', price: 1500 },
     { id: 3, name: 'Premium sound system', price: 500 },
     { id: 4, name: 'Rear spoiler', price: 250 }
   ]
-  export function additionalFeaturesReducer(additionalFeatures=initialAdditionalFeatures , action){
-    return additionalFeatures;
+};
+export function reducer(state=initialState, action){
+  switch(action.type){
+    case types.BUY_ITEM:
+      return{
+        ...state,
+            car:{
+              ...state.car,
+              features: [...state.car.features,{
+                name: action.payload.name,
+                price: action.payload.price
+              }
+              ],
+              price: state.car.price + action.payload.price
+            } 
+      }
+    case types.REMOVE_FEATURE:
+      return{
+        ...state,
+            car:{
+              ...state.car,
+              features: state.car.features.filter (item=> {
+                return item.id !== action.payload
+               } ),
+               price: state.car.price - action.payload.price
+            }
+
+      }
+      default:
+        return state;
   }
+
+}
